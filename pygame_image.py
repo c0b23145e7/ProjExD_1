4834efeaf0783e05b9630f5e6d5b90ee4d19ef8a
C0B23145 <c0b23145e7@edu.teu.ac.jp>
2024-09-24 15:46:21 +0900

    演習5 背景画像動かす＋間延び解消

--- pygame_image.py
+++ pygame_image.py
@@ -17,8 +17,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x = -(tmr%800)
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [x,0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
